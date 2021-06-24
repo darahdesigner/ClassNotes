2aa2f0ef83c7bc3634188dd05fee2932d570d200
Darah Snider <darahsnider@gmail.com>
2021-06-23 23:10:57 -0400

    scope

---
+++ javaScriptJune2021/1-jsFundamentals/1-grammarAndTypes/03-scope.js
@@ -0,0 +1,75 @@
+/* 
+SCOPE
+
+1-grammarAndTypes
+03-scope.js
+*/
+
+/*
+What is scope?
+
+JS has both LOCAL and GLOBAL scope
+*/
+
+var x = 12;
+//  global scope is available, ready from the get go, in leftmost edge of file
+function scope(){
+    var x = 33;
+    var y = 10;
+    console.log(x);
+}
+scope(); // logs 33
+console.log(x); // logs 12
+// console.log(y); //undefined
+
+// globally defined variable can be used inside or outside the function (anywhere in the file), but local variables can only be used inside the function0
+
+// example 2
+var x = 12;
+function scope (){
+    x=33;
+    console.log(x);
+}
+scope()//33
+console.log(x);//33
+
+//example 3:
+var x = 1;
+function scope (){
+    var x = 2;
+    function scopeInner (){
+        var x = 3;
+        console.log(x);//3
+    }
+    scopeInner();
+    console.log(x);//2
+}
+scope();
+console.log(x);//1
+
+//example 4 contrasts with 5:
+
+var x = 12;
+function scope(){
+    var x = 33;
+    if (true){
+        let x = 45;
+        console.log(x);// 45
+    } 
+    console.log(x)//33
+}
+scope();
+console.log(x);//12
+
+//example 5 contrasts with 4:
+var x = 12;
+function scope(){
+    var x = 33;
+    if (true){
+        var x = 45;
+        console.log(x);//45
+    }
+    console.log(x);//45 -> var doesnt obey "block" scope
+}
+scope();
+console.log(x);//12
